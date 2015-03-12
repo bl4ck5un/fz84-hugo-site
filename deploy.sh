a8547c04c4dbd2c87b9604ed7ed4681a90491899
Fan Zhang <bl4ck5unxx@gmail.com>
2015-03-12 10:16:14 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+PUBLIC=public
+hugo 
+
+if [ ! -d _deploy ]
+then
+    git clone https://github.com/bl4ck5un/bl4ck5un.github.io _deploy
+fi
+
+cd _deploy
+
+rm -rf *
+cp -rf ../$PUBLIC/* .
+find . -name "*.DS_Store" -type f -delete
+git add --all
+git commit -m "Site updated `date`"
+git push
+
+echo "Done"
+
